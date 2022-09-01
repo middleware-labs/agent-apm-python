a8e01883466f8488564f89f8aba7d6812e05663f
Nidhi-Middleware0829 <nidhi@middleware.io>
2022-09-01 19:08:07 +0530

    apmpython

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="apmpython",
-    version="0.1",
+    version="0.1.1",
     author="Nidhi_Sharma",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
     long_description=open('README.txt').read(),
