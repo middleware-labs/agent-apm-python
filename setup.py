b03c722049a62b00da02347a9d95a06bcb92941e
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2022-09-02 15:30:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1.2",
     author="Nidhi_Sharma",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/plain',
     packages=["apmpython"],
     url = "https://github.com/middleware-labs/agent-apm-python.git",
