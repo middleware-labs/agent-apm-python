d6c33330055eb180a4f33448983936bff0b4fa5d
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2022-09-03 12:44:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="mw-python-apm",
-    version="1.0.0",
-    author="Nidhi_Sharma",
+    version="1.0.1",
+    author="middleware-dev",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
     long_description=open('README.md').read(),
     long_description_content_type='text/plain',
