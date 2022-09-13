bd85d2f5a815aa53c900fd300aebce979df4ae53
Nidhi-Middleware0829 <nidhi@middleware.io>
2022-09-13 18:43:03 +0530

    Update logs

--- apmpythonpackage/apmpythonpackage.py
+++ apmpythonpackage/apmpythonpackage.py
@@ -5,24 +5,25 @@ import time
 import threading
 import gc
 from sys import getswitchinterval
-from ddtrace.runtime import RuntimeMetrics
-RuntimeMetrics.enable()
+from fluent import sender
+# from ddtrace.runtime import RuntimeMetrics
+# RuntimeMetrics.enable()
 
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
-from opentelemetry.metrics import get_meter_provider
-from opentelemetry.metrics import set_meter_provider
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from prometheus_client import Gauge
-from opentelemetry.metrics import get_meter
+# from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+# from opentelemetry.metrics import get_meter_provider
+# from opentelemetry.metrics import set_meter_provider
+# from opentelemetry.sdk.metrics import MeterProvider
+# from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+# from prometheus_client import Gauge
+# from opentelemetry.metrics import get_meter
 
 
-exporter = OTLPMetricExporter(insecure=True)
-reader = PeriodicExportingMetricReader(exporter)
-provider = MeterProvider(metric_readers=[reader])
-set_meter_provider(provider)
+# exporter = OTLPMetricExporter(insecure=True)
+# reader = PeriodicExportingMetricReader(exporter)
+# provider = MeterProvider(metric_readers=[reader])
+# set_meter_provider(provider)
 
-meter = get_meter_provider().get_meter("sample")
+# meter = get_meter_provider().get_meter("sample")
 
 class apmpythonclass:  
     def cpu_usage(self):
@@ -73,3 +74,8 @@ class apmpythonclass:
             tracker.context_switch()
             time.sleep(5)
             print("--------------------------------------------")
+
+    
+    def logemit(arg1, arg2):
+        logger = sender.FluentSender('app', host='localhost', port=8006)
+        logger.emit(arg1, arg2)
