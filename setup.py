4e1e98e54019fc72e65fab7289612b846f8a45f9
Nidhi-Middleware0829 <nidhi@middleware.io>
2022-09-13 20:56:28 +0530

    Update log

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 import setuptools
 
 setuptools.setup(
-<<<<<<< HEAD
-    name="mw-python-apm",
-    version="0.0.9",
-=======
+
     name="apmpython",
-<<<<<<< HEAD
-    version="0.1.9",
-=======
-    version="0.1.8",
->>>>>>> 7a3d3327e25943e26a1ffeb77fdd0f294d0797b1
->>>>>>> 0d2e6a61b4395dfcc93fe6b00ca9fd5e5608a69e
+    version="0.1.10",
+
     author="Nidhi_Sharma",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
     long_description=open('README.md').read(),
