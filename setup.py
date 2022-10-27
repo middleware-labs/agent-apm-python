48294d2c3eb73de79017fe4e5b0cb2d0d50f168d
ACQ053 <keval@middleware.io>
2022-10-27 14:41:56 +0530

    python package new version uploade

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
 
-    name="apmpython",
-    version="0.1.11",
+    name="middleware-apm",
+    version="0.1.6",
 
-    author="Nidhi_Sharma",
+    author="middleware-dev",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
     long_description=open('README.md').read(),
     long_description_content_type='text/plain',
