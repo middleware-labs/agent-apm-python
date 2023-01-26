1cfe9bd53f8c4df1b82f36eb14a98ccb85ac2d57
Keval Bhogayata <keval@middleware.io>
2023-01-26 21:50:02 +0530

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
 setuptools.setup(
 
     name="middleware-apm",
-    version="0.1.27",
-
+    version="0.1.29",
+    install_requires=requirements,
     author="middleware-dev",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
     long_description=open('README.md').read(),
