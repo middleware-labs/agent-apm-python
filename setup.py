c2a20145a130d5b6852d1516acb1b66bde73c787
Keval Bhogayata <keval@middleware.io>
2023-01-26 11:18:48 +0530

    workflow updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
 
     name="middleware-apm",
-    version="0.1.18",
+    version="0.1.19",
 
     author="middleware-dev",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
