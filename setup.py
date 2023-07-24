bb4c8cbc5c721726b7b63cdb0677b460a024d83d
Sanjay Suthar <21999586+sanjoyment@users.noreply.github.com>
2023-07-24 14:26:33 +0530

    Description beautifier (#35)
    * Revamped the flow and added Metrics functionality
    
    * Renamed dir from src to source
    
    * Flow Restructured
    
    * Flow Restructured
    
    * Flow Restructured
    
    * RC version increased
    
    * 0.2.0 submitted.
    
    * Description beautifier

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ except IOError:
 setuptools.setup(
 
     name="middleware-apm",
-    version="0.2.0",
+    version="0.2.1",
     install_requires=requirements,
     author="middleware-dev",
-    description="This package is use to check the RAM and CPU Usage of Current Device.",
+    description="Middleware's APM tool enables Python developers to effortlessly monitor their applications, gathering distributed tracing, metrics, logs, and profiling data for valuable insights and performance optimization. Install the Middleware Host agent and integrate the APM package to leverage its powerful functionalities.",
     long_description=open('README.md').read(),
-    long_description_content_type='text/plain',
+    long_description_content_type='text/markdown',
     packages=["mw_tracker"],
     url = "https://github.com/middleware-labs/agent-apm-python.git",
     entry_points={
