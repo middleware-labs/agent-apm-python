6b148de430691f769d91de65f3bc560dcc72bc3a
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-10-03 18:58:08 +0530

    Revamped Python-APM flow

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ packages = [
 
 setuptools.setup(
     name="middleware-apm",
-    version="0.2.2rc11",
+    version="0.2.2rc12",
     install_requires=requirements,
     author="middleware-dev",
     maintainer="middleware-dev",
