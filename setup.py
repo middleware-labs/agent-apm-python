34020976114fa1232ac74e7d2d2b75f4c0081344
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-11-01 10:57:32 +0530

    v0.3.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ packages = [
 
 setuptools.setup(
     name="middleware-apm",
-    version="0.3.1rc1",
+    version="0.3.2",
     install_requires=requirements,
     author="middleware-dev",
     maintainer="middleware-dev",
